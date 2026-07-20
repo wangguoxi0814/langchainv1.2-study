77e7aa56d6e0bd3d8ec2e3fd69ff43a72a64241b
wangguoxi <wangguoxi@darryring.com>
2026-07-20 22:22:11 +0800

    feat: Pydantic和模型的结构化输出

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 
-def init_simple_qwen_max(**kwargs):
+def init_simple_qwen_max():
     """
     获取qwen-max模型
     :return:
@@ -16,6 +16,31 @@ def init_simple_qwen_max(**kwargs):
 
     model = init_chat_model(
         model='qwen-max',
+        model_provider='openai',
+        api_key=DASHSCOPE_API_KEY,
+        base_url=DASHSCOPE_BASE_URL,
+    )
+
+    return model
+
+
+def init_simple_dashscope_model(model):
+    """
+    获取DASHSCOPE模型
+    :return:
+    """
+    from langchain.chat_models import init_chat_model
+    from dotenv import load_dotenv
+    import os
+    from pathlib import Path
+
+    # 从.env文件中加载环境变量
+    load_dotenv(Path('../.env'), override=True)
+    DASHSCOPE_API_KEY = os.getenv("DASHSCOPE_API_KEY")
+    DASHSCOPE_BASE_URL = os.getenv("DASHSCOPE_BASE_URL")
+
+    model = init_chat_model(
+        model=model,
         model_provider='openai',
         api_key=DASHSCOPE_API_KEY,
         base_url=DASHSCOPE_BASE_URL,
